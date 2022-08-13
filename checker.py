14f8e5bf5da6aacd44a13283b2501ae009fbad61
Exanys <binarvojtech@gmail.com>
2022-08-13 10:20:21 +0100

    edited_to_class

---
+++ checker.py
@@ -0,0 +1,40 @@
+from validate_email_address import validate_email
+import pandas as pd
+import time
+
+
+class Checker:
+    file = ''
+    result_name = ''
+    columns = []
+    email_col = ''
+    valid = []
+    invalid = []
+
+    def __init__(self, file: str, result_name: str, columns: list, email_col: str):
+        self.file = file
+        self.result_name = result_name
+        self.columns = columns
+        self.email_col = email_col
+
+    def get_data(self):
+        excel_data = pd.read_excel(self.file)
+        data = pd.DataFrame(excel_data, columns=self.columns).to_dict('list')
+        return data
+
+    def validate(self, data: list):
+        for i in range(0, len(data[self.email_col])):
+            if str(data[self.email_col][i]) != 'nan' and str(data[self.email_col][i]) != 'Neuvedeno':
+                # print(data[self.email_col][i])
+                if validate_email(data[self.email_col][i], verify=True):
+                    self.valid.append([data[self.columns[0]][i], data[self.email_col][i], data[self.columns[2]][i]])
+                else:
+                    self.invalid.append(data[self.email_col][i])
+        self.valid.sort(key=lambda x: x[1])
+
+    def save(self):
+        to_write = pd.DataFrame(self.valid, columns=self.columns)
+        to_write.to_excel(self.result_name, sheet_name='Valid e-mails', index=False)
+        print(to_write)
+        print(f'File {self.result_name} has been created!')
+
--- main.py
+++ main.py
@@ -1,23 +1,12 @@
-from validate_email_address import validate_email
-import pandas as pd
-import time
-FILE_NAME = 'hafio_hlidaci_emaily.xlsx'
-# FILE_NAME = 'Databaze_firem_2021.xlsx'
-RESULT_FILE = 'result.xlsx'
-excel_data = pd.read_excel(FILE_NAME)
-# data = pd.DataFrame(excel_data, columns=['E-mail']).to_dict('list')
-data = pd.DataFrame(excel_data, columns=['Emaily']).to_dict('list')
-valid = []
-invalid = []
-# for i in range(0, len(data['E-mail'])):
-#     if validate_email(data['E-mail'][i], verify=True):
-#         valid.append(data['E-mail'][i])
-#     else:
-#         invalid.append(data['E-mail'][i])
-#     time.sleep(1)
-for i in range(0, len(data['Emaily'])):
-    valid.append(data['Emaily'][i]) if validate_email(data['Emaily'][i], verify=True) else invalid.append(data['Emaily'][i])
-    time.sleep(0.5)
+from checker import Checker
 
-to_write = pd.DataFrame(valid, columns=['Valid e-mails'])
-to_write.to_excel(RESULT_FILE, sheet_name='Valid e-mails', index=False)
+
+def run():
+    checker = Checker('All_contacts.xlsx', 'result_krasotky.xlsx', ['Jméno', 'Email', 'Číslo'], 'Email')
+    data = checker.get_data()
+    checker.validate(data)
+    checker.save()
+
+
+if __name__ == '__main__':
+    run()
