64fb5ff1e1a6a1e79fd2c0af15a680cc32f27033
Exanys <binarvojtech@gmail.com>
2022-08-19 11:00:11 +0100

    email_template

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from checker import Checker
 from sendEmails import Mail
 
-START = 10
+START = 50
 def run():
+
+    # scrapper
     # checker = Checker('All_contacts.xlsx', 'result_krasotky.xlsx', ['Jméno', 'Email', 'Číslo'], 'Email')
     # data = checker.get_data()
     # checker.validate(data)
     # checker.save()
-    mailer = Checker('result_krasotky.xlsx', '', ['Email'], 'Email')
-    emails = mailer.get_data()['Email']
-    mail = Mail()
-    mail = mail.send(emails[START:START + 10], 'Nový portál pro erotickou inzerci zdarma')
-    print(mail)
+
+    # production
+    # mailer = Checker('result_krasotky.xlsx', '', ['Email'], 'Email')
+    # emails = mailer.get_data()['Email']
     # mail = Mail()
-    # mail = mail.send(['testovaci226@gmail.com'], 'Nový portál pro erotickou inzerci zdarma')
+    # mail = mail.send(emails[START:START + 20], 'Nový portál pro erotickou inzerci zdarma')
     # print(mail)
+
+    # for test emails
+    mail = Mail()
+    mail = mail.send(['testovaci226@gmail.com'], 'Nový portál pro erotickou inzerci zdarma')
+    print(mail)
 
 
 
